1a1a8fa093f7ffecf0203992bef3718b6b4c08d3
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-10-03 20:17:32 +0600

    Problem -02 ( Cycle or Laptop ) || added

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ if (0 < area && area <= 10 ** 9) {
   var result1 = area / 2;
   console.log(result1);
 }
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (0 < money && money <= 10 ** 9) {
+  if (money >= 25000) {
+    console.log("Laptop");
+  } else if (10000 <= money && money < 25000) {
+    console.log("Cycle");
+  } else {
+    console.log("Chocolate");
+  }
+}
